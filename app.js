8d596fa91ba662e4ac908af31ebe5f0cd9553ad9
Sam Kersebet <sam.kersebet@gmail.com>
2021-01-19 17:42:52 -0600

    added ejs boilerplate dependencies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
@@ -19,9 +20,9 @@ db.once('open', () => {
 const app = express();
 const port = 3000;
 
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', ejsMate); 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
